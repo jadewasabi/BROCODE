da55afcb16d9f5c784fdece513277cc267b6b52a
jadewasabi <jadewasabi449@gmail.com>
2026-07-19 01:06:32 +0800

    Add files via upload

---
+++ pages/api/posts/[id]/comment.js
@@ -0,0 +1,6 @@
+import orig from '../../../../../../api/posts-comment.js';
+
+export default function handler(req, res) {
+  return orig(req, res);
+}
+
---
+++ pages/api/posts/[id]/react.js
@@ -0,0 +1,6 @@
+import orig from '../../../../../../api/posts-react.js';
+
+export default function handler(req, res) {
+  return orig(req, res);
+}
+
