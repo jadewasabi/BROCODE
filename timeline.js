cb89f2de4cb45a068a9b437066c1d583982acd5c
jadewasabi <jadewasabi449@gmail.com>
2026-07-19 22:24:28 +0800

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -4,7 +4,7 @@
 // TODO: set this to your Vercel app origin (leave trailing slash off).
 // If you deploy to Vercel, set it to: https://<YOUR_VERCE_L_APP>.vercel.app
 // Use same origin so it works regardless of your Vercel URL
-const API_BASE = 'https://jadexbun/';
+const API_BASE = 'https://jadexbun-aid-4-prog.vercel.app'; // no trailing slash
 
 
 
