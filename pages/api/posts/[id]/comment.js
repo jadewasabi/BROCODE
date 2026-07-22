cfbc52b70d7e77cfac1221cc2d03ea251a64bf74
jadewasabi <jadewasabi449@gmail.com>
2026-07-22 19:52:14 +0800

    Update comment.js

--- pages/api/posts/[id]/comment.js
+++ pages/api/posts/[id]/comment.js
@@ -1,11 +1,6 @@
-import orig from '../../../../../api/posts-comment.js';
+import orig from '../../../../../api/posts/[id]/comment.js';
 
 export default function handler(req, res) {
-  // Inject postId from the URL parameter into the request body
-  if (req.query && req.query.id) {
-    req.body = req.body || {};
-    req.body.postId = req.query.id;
-  }
   return orig(req, res);
 }
 
